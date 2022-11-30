a5049077990f666833e35cb41daea131ae05cfe4
Toyin <toyincodetech@gmail.com>
2022-11-30 03:02:42 -0800

    Task 5

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+comma = ","
+for n in range(0, 100):
+    if n < 10:
+        print("{}{}".format(0, n), end=comma)
+    else:
+        if n == 99:
+            comma = "\n"
+        print("{}".format(n), end=comma) # or print(n, end=comma)
