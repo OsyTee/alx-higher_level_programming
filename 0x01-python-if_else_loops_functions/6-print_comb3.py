99492f3c0986937b3f0a6309a3f1141ab983d4dc
Toyin <toyincodetech@gmail.com>
2022-12-01 02:47:18 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,11 @@
-comma = ","
 for d in range(1, 100):
     digit_1 = d/10
     digit_2 = d%10
     if d < 10 and digit_1 < digit_2:
-        print("{}{}".format(0, d), end=comma)
+        print("{}{}".format(0, d), end=",")
     elif digit_1 < digit_2:
         if d == 89:
-            comma = "\n"
-            print(d, end=comma)
+            break
+            print(d, end=",")
 
 
