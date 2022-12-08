5a0833a74e85d0c5a6f916359672e70718c27142
Toyin <toyincodetech@gmail.com>
2022-12-08 03:40:12 -0800

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    suma = 0
+    i = 0
+    for a in sys.argv:
+        if i > 0:
+            suma += int(a)
+        i += 1
+    print(suma)
