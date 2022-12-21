cf0306dfe87c4eac28db0f01f5ef0afc0e0d074d
Toyin <toyincodetech@gmail.com>
2022-12-21 06:46:18 -0800

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,17 +5,16 @@
 class Square:
     """Represent a square
     Attributes:
-        __size(int): size of a side of the square
+    __size(int): size of a side of the square
     """
-
-    def __init__(self, size=0):\
-    """initialize square
-            Args:
-                size (int): size of a side of the square
-                Returns:
-                None
-            """
-       if type(size) is not int:
+    def __init__(self, size=0):
+        """initialize square
+        Args:
+        size (int): size of a side of the square
+        Returns:
+        None
+        """
+        if type(size) is not int:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
