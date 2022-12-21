38082e70c023b686c680bca894fb75bace538f27
Toyin <toyincodetech@gmail.com>
2022-12-21 07:03:28 -0800

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,22 +5,22 @@
 class Square:
     """Represents a square
     Attributes:
-    __size (int): size of a side of the square
+        __size (int): size of a side of the square
     """
     def __init__(self, size=0):
         """initializes the square
         Args:
-        size (int): size of a side of the square
+            size (int): size of a side of the square
         Returns:
-        None
+            None
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
             else:
-                if size < 0:
-                    raise ValueError("size must be >= 0")
-                    else:
-                        self.__size = size
+                self.__size = size
 
     def area(self):
         """calculates the square's area
