9e57e73cf5b09b231747e2e550acc0f14062d306
OsyTee <OsyTee@github.com>
2023-05-04 05:01:03 -0700

    Task 9

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $.get('https://swapi.co/api/films/?format=json', function (data) {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
   $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
   });
 });
